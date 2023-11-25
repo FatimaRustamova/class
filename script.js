20e112d8ec15d62294f6d3a32e0f811578f97c30
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-11-25 11:31:55 +0400

    Class

---
+++ script.js
@@ -0,0 +1,60 @@
+// let div1 = document.querySelector("#div1");
+// let div2 = document.querySelector("#div2");
+// let div3 = document.querySelector("#div3");
+// let div4 = document.querySelector("#div4");
+// let div5 = document.querySelector("#div5");
+
+// div1.addEventListener("click", ()=>{
+//     div1.style.backgroundColor = "bisque";
+//     div2.style.backgroundColor = "";
+//     div3.style.backgroundColor = "";
+//     div4.style.backgroundColor = "";
+//     div5.style.backgroundColor = "";
+// })
+
+// div2.addEventListener("click", ()=>{
+//     div2.style.backgroundColor = "bisque";
+//     div1.style.backgroundColor = "bisque";
+//     div3.style.backgroundColor = "";
+//     div4.style.backgroundColor = "";
+//     div5.style.backgroundColor = "";
+// })
+
+// div3.addEventListener("click", ()=>{
+//     div3.style.backgroundColor = "bisque";
+//     div1.style.backgroundColor = "bisque";
+//     div2.style.backgroundColor = "bisque";
+//     div4.style.backgroundColor = "";
+//     div5.style.backgroundColor = "";
+// })
+
+// div4.addEventListener("click", ()=>{
+//     div4.style.backgroundColor = "bisque";
+//     div1.style.backgroundColor = "bisque";
+//     div2.style.backgroundColor = "bisque";
+//     div3.style.backgroundColor = "bisque";
+//     div5.style.backgroundColor = "";
+// })
+
+// div5.addEventListener("click", ()=>{
+//     div5.style.backgroundColor = "bisque";
+//     div1.style.backgroundColor = "bisque";
+//     div2.style.backgroundColor = "bisque";
+//     div3.style.backgroundColor = "bisque";
+//     div4.style.backgroundColor = "bisque";
+// })
+
+// let div = document.querySelectorAll("div");
+
+// div.forEach(item=>{
+//     item.addEventListener("click", function(e){
+//         div.forEach(item=>{
+//             item.style.backgroundColor = "";
+//         })
+//         div.forEach(item=>{
+//             if(e.target.style.backgroundColor !== "yellow"){
+//                 item.style.backgroundColor = "yellow";
+//             }
+//         })
+//     })
+// })
